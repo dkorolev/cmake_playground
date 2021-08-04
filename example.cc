744df301f7faf334d48fc7a3072d1e6e4a1f8b1f
Dima <dmitry.korolev@gmail.com>
2021-08-04 13:02:50 +0300

    Ran `make indent`.

--- example.cc
+++ example.cc
@@ -2,6 +2,4 @@
 
 #include "header.h"
 
-int main() {
-  std::cout << "2 + 5 is " << sum(2, 5) << ", and 2 * 5 is " << mul(2, 5) << std::endl;
-}
+int main() { std::cout << "2 + 5 is " << sum(2, 5) << ", and 2 * 5 is " << mul(2, 5) << std::endl; }
